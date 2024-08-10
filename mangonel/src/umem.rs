170227e0b8da87cc7553b4011ad9b3e073a2ebe2
kanet <zombiedelah@gmail.com>
2024-08-10 14:34:37 +0900

    Initial Umem APIs

--- mangonel/src/umem.rs
+++ mangonel/src/umem.rs
@@ -1,22 +1,59 @@
-use std::ptr::NonNull;
+use std::{ptr::NonNull, sync::Arc};
 
-use mangonel_libxdp_sys::{xsk_umem, xsk_umem__create, xsk_umem_config};
+use mangonel_libxdp_sys::{xsk_umem, xsk_umem__create, xsk_umem__delete, xsk_umem_config};
 
 use crate::{
     mmap::Mmap,
-    ring::{CompletionRing, FillRing},
+    ring::{CompletionRing, FillRing, RingError},
 };
 
-pub struct Umem(NonNull<xsk_umem>);
+pub struct Umem {
+    inner: Arc<UmemInner>,
+}
+
+struct UmemInner {
+    mmap: Mmap,
+    completion_ring: CompletionRing,
+    fill_ring: FillRing,
+    umem: NonNull<xsk_umem>,
+}
+
+impl Drop for UmemInner {
+    fn drop(&mut self) {
+        let value = unsafe { xsk_umem__delete(self.umem.as_ptr()) };
+
+        if value.is_negative() {
+            panic!(
+                "{:?}",
+                UmemError::CleanUp(std::io::Error::from_raw_os_error(-value))
+            );
+        }
+    }
+}
+
+unsafe impl Send for Umem {}
+
+unsafe impl Sync for Umem {}
+
+impl Clone for Umem {
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+        }
+    }
+}
 
 impl Umem {
     pub fn initialize(
-        mmap: &Mmap,
-        completion_ring: &CompletionRing,
-        fill_ring: &FillRing,
+        mmap: Mmap,
+        completion_ring_size: u32,
+        fill_ring_size: u32,
         frame_size: u32,
         frame_headroom: u32,
     ) -> Result<Self, UmemError> {
+        let completion_ring = CompletionRing::uninitialized(completion_ring_size)?;
+        let fill_ring = FillRing::uninitialized(fill_ring_size)?;
+
         let umem_config = xsk_umem_config {
             fill_size: fill_ring.size(),
             comp_size: completion_ring.size(),
@@ -38,23 +75,45 @@ impl Umem {
         };
 
         if value.is_negative() {
-            return Err(value.into());
+            return Err(UmemError::Initialize(std::io::Error::from_raw_os_error(
+                -value,
+            )));
         }
 
-        Ok(Self(umem))
+        let umem_inner = UmemInner {
+            mmap,
+            completion_ring,
+            fill_ring,
+            umem,
+        };
+
+        Ok(Self {
+            inner: Arc::new(umem_inner),
+        })
+    }
+
+    pub fn mmap(&self) -> &Mmap {
+        &self.inner.mmap
+    }
+
+    pub fn completion_ring(&self) -> &CompletionRing {
+        &self.inner.completion_ring
+    }
+
+    pub fn fill_ring_as_ptr(&self) -> &FillRing {
+        &self.inner.fill_ring
     }
 
     pub fn as_ptr(&self) -> *mut xsk_umem {
-        self.0.as_ptr()
+        self.inner.umem.as_ptr()
     }
 }
 
-pub struct UmemError(std::io::Error);
-
-impl std::fmt::Debug for UmemError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Failed to initialize Umem: {:?}", self.0)
-    }
+#[derive(Debug)]
+pub enum UmemError {
+    RingSize(RingError),
+    Initialize(std::io::Error),
+    CleanUp(std::io::Error),
 }
 
 impl std::fmt::Display for UmemError {
@@ -65,8 +124,8 @@ impl std::fmt::Display for UmemError {
 
 impl std::error::Error for UmemError {}
 
-impl From<i32> for UmemError {
-    fn from(value: i32) -> Self {
-        Self(std::io::Error::from_raw_os_error(-value))
+impl From<RingError> for UmemError {
+    fn from(value: RingError) -> Self {
+        Self::RingSize(value)
     }
 }
