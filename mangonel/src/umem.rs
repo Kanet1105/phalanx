5411859658646064a8f9d541661aaa2ad10e3cb6
Kanet <zombiedelah@gmail.com>
2024-08-20 17:41:03 +0900

    Minimal wrapper of 'xdp_umem'

--- mangonel/src/umem.rs
+++ mangonel/src/umem.rs
@@ -6,8 +6,7 @@ use std::{
 use mangonel_libxdp_sys::{xsk_umem, xsk_umem__create, xsk_umem__delete, xsk_umem_config};
 
 use crate::{
-    frame::Frame,
-    mmap::Mmap,
+    mmap::{Mmap, MmapError},
     ring::{CompletionRing, FillRing, RingError},
 };
 
@@ -31,18 +30,23 @@ impl Clone for Umem {
 
 impl Umem {
     pub fn new(
+        frame_size: u32,
+        headroom_size: u32,
+        descriptor_count: u32,
         completion_ring_size: u32,
         fill_ring_size: u32,
-        mmap: &Mmap,
+        use_hugetlb: bool,
     ) -> Result<Self, UmemError> {
+        let mmap = Mmap::new(frame_size, headroom_size, descriptor_count, use_hugetlb)?;
+
         let mut umem_ptr = null_mut::<xsk_umem>();
         let mut completion_ring = CompletionRing::uninitialized(completion_ring_size)?;
         let mut fill_ring = FillRing::uninitialized(fill_ring_size)?;
         let umem_config = xsk_umem_config {
             fill_size: fill_ring_size,
             comp_size: completion_ring_size,
-            frame_size: mmap.frame_size(),
-            frame_headroom: mmap.frame_headroom_size(),
+            frame_size,
+            frame_headroom: headroom_size,
             flags: 0,
         };
 
@@ -57,15 +61,16 @@ impl Umem {
             )
         };
         if value.is_negative() {
-            return Err(UmemError::InitializeUmem(
-                std::io::Error::from_raw_os_error(-value),
-            ));
+            return Err(UmemError::Initialize(std::io::Error::from_raw_os_error(
+                -value,
+            )));
         }
 
         let inner = UmemInner {
             umem: NonNull::new(umem_ptr).ok_or(UmemError::UmemIsNull)?,
             completion_ring: completion_ring.initialize()?,
             fill_ring: fill_ring.initialize()?,
+            mmap,
         };
 
         Ok(Self(Arc::new(inner)))
@@ -76,6 +81,7 @@ pub struct UmemInner {
     umem: NonNull<xsk_umem>,
     completion_ring: CompletionRing,
     fill_ring: FillRing,
+    mmap: Mmap,
 }
 
 impl Drop for UmemInner {
@@ -89,28 +95,13 @@ impl Drop for UmemInner {
         if value.is_negative() {
             panic!(
                 "{:?}",
-                UmemError::FreeUmem(std::io::Error::from_raw_os_error(-value))
+                UmemError::Free(std::io::Error::from_raw_os_error(-value))
             );
         }
     }
 }
 
 impl UmemInner {
-    pub fn populate(&self, frame_size: u32, frame_headroom_size: u32, descriptor_count: u32) {
-        let mut index: u32 = 0;
-        let frame_size = frame_size + frame_headroom_size;
-
-        let available = self.fill_ring.reserve(descriptor_count, &mut index);
-        for descriptor_index in 0..available {
-            // Set the descriptor address for each index in the fill ring.
-            let descriptor_address = self.fill_ring.fill_address(index);
-            unsafe { *descriptor_address = (descriptor_index * frame_size).try_into().unwrap() }
-            index += 1;
-        }
-
-        self.fill_ring.submit(descriptor_count);
-    }
-
     #[inline(always)]
     pub fn as_ptr(&self) -> *mut xsk_umem {
         self.umem.as_ptr()
@@ -126,18 +117,19 @@ impl UmemInner {
         &self.fill_ring
     }
 
-    // #[inline(always)]
-    // pub fn mmap(&self) -> &Mmap {
-    //     &self.mmap
-    // }
+    #[inline(always)]
+    pub fn mmap(&self) -> &Mmap {
+        &self.mmap
+    }
 }
 
 #[derive(Debug)]
 pub enum UmemError {
+    Mmap(MmapError),
     Ring(RingError),
-    InitializeUmem(std::io::Error),
+    Initialize(std::io::Error),
     UmemIsNull,
-    FreeUmem(std::io::Error),
+    Free(std::io::Error),
 }
 
 impl std::fmt::Display for UmemError {
@@ -147,6 +139,12 @@ impl std::fmt::Display for UmemError {
 }
 
 impl std::error::Error for UmemError {}
+
+impl From<MmapError> for UmemError {
+    fn from(value: MmapError) -> Self {
+        Self::Mmap(value)
+    }
+}
 
 impl From<RingError> for UmemError {
     fn from(value: RingError) -> Self {
