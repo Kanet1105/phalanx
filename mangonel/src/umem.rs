8c7893cf05b1586db22b91720ecad09c816dcc58
Kanet <zombiedelah@gmail.com>
2024-08-26 14:31:57 +0900

    Stop dereferencing the inner type

--- mangonel/src/umem.rs
+++ mangonel/src/umem.rs
@@ -1,32 +1,55 @@
 use std::{
-    collections::VecDeque,
     ptr::{null_mut, NonNull},
     sync::Arc,
 };
 
+use crossbeam::queue::ArrayQueue;
 use mangonel_libxdp_sys::{xsk_umem, xsk_umem__create, xsk_umem__delete, xsk_umem_config};
 
 use crate::{
-    frame::Descriptor,
     mmap::{Mmap, MmapError},
     ring::{ConsumerRing, ProducerRing, RingError, RingType},
 };
 
-pub struct Umem(Arc<UmemInner>);
+pub struct Umem {
+    inner: Arc<UmemInner>,
+}
 
-impl std::ops::Deref for Umem {
-    type Target = UmemInner;
+struct UmemInner {
+    umem: NonNull<xsk_umem>,
+    completion_ring: ConsumerRing,
+    fill_ring: ProducerRing,
+    buffer_free: ArrayQueue<u64>,
+    mmap: Mmap,
+}
 
-    #[inline(always)]
-    fn deref(&self) -> &Self::Target {
-        &self.0
+impl Drop for UmemInner {
+    /// # Panics
+    ///
+    /// The program panics when it fails to clean up. This is not a problem
+    /// while it is running and each [`RxSocket`] and [`TxSocket`] is referring
+    /// to it. However, we want to see the error when it happens.
+    fn drop(&mut self) {
+        let value = unsafe { xsk_umem__delete(self.umem.as_ptr()) };
+        if value.is_negative() {
+            panic!(
+                "{:?}",
+                UmemError::Free(std::io::Error::from_raw_os_error(-value))
+            );
+        }
     }
 }
+
+unsafe impl Send for Umem {}
+
+unsafe impl Sync for Umem {}
 
 impl Clone for Umem {
     #[inline(always)]
     fn clone(&self) -> Self {
-        Self(self.0.clone())
+        Self {
+            inner: self.inner.clone(),
+        }
     }
 }
 
@@ -35,15 +58,15 @@ impl Umem {
         frame_size: u32,
         headroom_size: u32,
         descriptor_count: u32,
-        completion_ring_size: u32,
         fill_ring_size: u32,
+        completion_ring_size: u32,
         use_hugetlb: bool,
     ) -> Result<Self, UmemError> {
         let mmap = Mmap::new(frame_size, headroom_size, descriptor_count, use_hugetlb)?;
 
         let mut umem_ptr = null_mut::<xsk_umem>();
-        let mut completion_ring = RingType::completion_ring_uninit(completion_ring_size)?;
         let mut fill_ring = RingType::fill_ring_uninit(fill_ring_size)?;
+        let mut completion_ring = RingType::completion_ring_uninit(completion_ring_size)?;
         let umem_config = xsk_umem_config {
             fill_size: fill_ring_size,
             comp_size: completion_ring_size,
@@ -68,92 +91,87 @@ impl Umem {
             )));
         }
 
+        // Prefill the buffer with addresses.
+        let buffer_free = ArrayQueue::new(descriptor_count as usize);
+        (0..descriptor_count).for_each(|descriptor_index: u32| {
+            let offset = descriptor_index * (frame_size + headroom_size);
+            buffer_free.push(offset as u64).unwrap();
+        });
+
         let inner = UmemInner {
             umem: NonNull::new(umem_ptr).ok_or(UmemError::UmemIsNull)?,
-            completion_ring: completion_ring.init()?,
             fill_ring: fill_ring.init()?,
+            completion_ring: completion_ring.init()?,
+            buffer_free,
             mmap,
         };
+        let umem = Self {
+            inner: Arc::new(inner),
+        };
 
-        Ok(Self(Arc::new(inner)))
+        // Populate the fill ring.
+        umem.fill();
+
+        Ok(umem)
     }
-}
 
-pub struct UmemInner {
-    umem: NonNull<xsk_umem>,
-    completion_ring: ConsumerRing,
-    fill_ring: ProducerRing,
-    mmap: Mmap,
-}
-
-impl Drop for UmemInner {
-    /// # Panics
-    ///
-    /// The program panics when it fails to clean up. This is not a problem
-    /// while it is running and each [`RxSocket`] and [`TxSocket`] is referring
-    /// to it. However, we want to see the error when it happens.
-    fn drop(&mut self) {
-        let value = unsafe { xsk_umem__delete(self.umem.as_ptr()) };
-        if value.is_negative() {
-            panic!(
-                "{:?}",
-                UmemError::Free(std::io::Error::from_raw_os_error(-value))
-            );
-        }
-    }
-}
-
-impl UmemInner {
     #[inline(always)]
     pub fn as_ptr(&self) -> *mut xsk_umem {
-        self.umem.as_ptr()
-    }
-
-    #[inline(always)]
-    pub fn completion_ring(&self) -> &ConsumerRing {
-        &self.completion_ring
+        self.inner.umem.as_ptr()
     }
 
     #[inline(always)]
     pub fn fill_ring(&self) -> &ProducerRing {
-        &self.fill_ring
+        &self.inner.fill_ring
+    }
+
+    #[inline(always)]
+    pub fn completion_ring(&self) -> &ConsumerRing {
+        &self.inner.completion_ring
     }
 
     #[inline(always)]
     pub fn mmap(&self) -> &Mmap {
-        &self.mmap
+        &self.inner.mmap
     }
 
     #[inline(always)]
-    pub fn fill(&self, buffer: &mut VecDeque<Descriptor>) -> u32 {
+    pub fn fill(&self) -> u32 {
         let mut index: u32 = 0;
-        let descriptor_count = buffer.len() as u32;
+        let size = self.inner.buffer_free.len() as u32;
 
-        let available = self.fill_ring.reserve(descriptor_count, &mut index);
+        let available = self.inner.fill_ring.reserve(size, &mut index);
         if available > 0 {
             for _ in 0..available {
-                let address = self.fill_ring.fill_address(index);
-                unsafe { *address = buffer.pop_front().unwrap().address() };
+                let address = self.inner.fill_ring.fill_address(index);
+                unsafe { *address = self.inner.buffer_free.pop().unwrap() }
                 index += 1;
             }
 
-            self.fill_ring.submit(available);
+            self.inner.fill_ring.submit(available);
         }
 
         available
     }
 
-    // pub fn complete(&self, buffer: &mut VecDeque<Descriptor>) {
-    //     let mut index: u32 = 0;
-    //     let descriptor_count = buffer.len() as u32;
+    #[inline(always)]
+    pub fn complete(&self) -> u32 {
+        let mut index: u32 = 0;
+        let size = self.inner.buffer_free.capacity() as u32;
 
-    //     let available = self.completion_ring.peek(descriptor_count, &mut index);
-    //     if available > 0 {
-    //         for _ in 0..available {}
+        let available = self.inner.completion_ring.peek(size, &mut index);
+        if available > 0 {
+            for _ in 0..available {
+                let address = self.inner.completion_ring.complete_address(index);
+                unsafe { self.inner.buffer_free.push(*address).unwrap() };
+                index += 1;
+            }
 
-    //         self.completion_ring.release(available);
-    //     }
-    // }
+            self.inner.completion_ring.release(available);
+        }
+
+        available
+    }
 }
 
 #[derive(Debug)]
