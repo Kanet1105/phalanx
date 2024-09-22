db473d9e30fa9725e194324f763b993b2d8dd74e
kanet <zombiedelah@gmail.com>
2024-09-22 21:42:38 +0900

    Linted and add descriptor buffer for rx and tx socket

--- mangonel/src/socket.rs
+++ mangonel/src/socket.rs
@@ -8,9 +8,8 @@ use std::{
 use libc::{poll, pollfd, sendto, MSG_DONTWAIT, POLLIN};
 use mangonel_libxdp_sys::{
     xsk_socket, xsk_socket__create, xsk_socket__delete, xsk_socket__fd, xsk_socket_config,
-    xsk_socket_config__bindgen_ty_1, XDP_COPY, XDP_ZEROCOPY, XSK_RING_CONS__DEFAULT_NUM_DESCS,
-    XSK_RING_PROD__DEFAULT_NUM_DESCS, XSK_UMEM__DEFAULT_FRAME_HEADROOM,
-    XSK_UMEM__DEFAULT_FRAME_SIZE,
+    xsk_socket_config__bindgen_ty_1, XDP_COPY, XDP_ZEROCOPY, XSK_RING_PROD__DEFAULT_NUM_DESCS,
+    XSK_UMEM__DEFAULT_FRAME_HEADROOM, XSK_UMEM__DEFAULT_FRAME_SIZE,
 };
 
 use crate::{
@@ -27,7 +26,6 @@ pub struct SocketBuilder {
     pub frame_headroom_size: u32,
     pub ring_size: u32,
     pub use_hugetlb: bool,
-    pub descriptor_count: u32,
     pub force_zero_copy: bool,
 }
 
@@ -38,7 +36,6 @@ impl Default for SocketBuilder {
             frame_headroom_size: XSK_UMEM__DEFAULT_FRAME_HEADROOM,
             ring_size: XSK_RING_PROD__DEFAULT_NUM_DESCS,
             use_hugetlb: false,
-            descriptor_count: XSK_RING_CONS__DEFAULT_NUM_DESCS * 2,
             force_zero_copy: false,
         }
     }
@@ -55,12 +52,11 @@ impl SocketBuilder {
     ) -> Result<(RxSocket, TxSocket), SocketError> {
         setrlimit();
 
-        let (rx_socket, tx_socket) = Socket::new(
+        let (rx_socket, tx_socket) = Socket::init(
             self.frame_size,
             self.frame_headroom_size,
             self.ring_size,
             self.use_hugetlb,
-            self.descriptor_count,
             self.force_zero_copy,
             interface_name,
             queue_id,
@@ -76,15 +72,15 @@ pub struct Socket {
 
 struct SocketInner(NonNull<xsk_socket>);
 
+unsafe impl Send for SocketInner {}
+
+unsafe impl Sync for SocketInner {}
+
 impl Drop for SocketInner {
     fn drop(&mut self) {
         unsafe { xsk_socket__delete(self.0.as_ptr()) }
     }
 }
-
-unsafe impl Send for Socket {}
-
-unsafe impl Sync for Socket {}
 
 impl Clone for Socket {
     #[inline(always)]
@@ -96,23 +92,16 @@ impl Clone for Socket {
 }
 
 impl Socket {
-    pub fn new(
+    pub fn init(
         frame_size: u32,
         frame_headroom_size: u32,
         ring_size: u32,
         use_hugetlb: bool,
-        descriptor_count: u32,
         force_zero_copy: bool,
         interface_name: impl AsRef<str>,
         queue_id: u32,
     ) -> Result<(RxSocket, TxSocket), SocketError> {
-        let umem = Umem::new(
-            frame_size,
-            frame_headroom_size,
-            ring_size,
-            descriptor_count,
-            use_hugetlb,
-        )?;
+        let umem = Umem::new(frame_size, frame_headroom_size, ring_size, use_hugetlb)?;
 
         let mut rx_ring = ConsumerRingUninit::new(ring_size)?;
         let mut tx_ring = ProducerRingUninit::new(ring_size)?;
@@ -158,10 +147,9 @@ impl Socket {
         };
 
         // Pre-fill the buffer with addresses.
-        let descriptor_buffer = DescriptorBuffer::<u64>::new(descriptor_count as usize);
-        (0..descriptor_count).for_each(|descriptor_index: u32| {
+        let descriptor_buffer = DescriptorBuffer::<u64>::new(ring_size as usize);
+        (0..ring_size).for_each(|descriptor_index: u32| {
             let offset = descriptor_index * (frame_headroom_size + frame_size);
-            println!("{}", offset);
             descriptor_buffer.push(offset as u64);
         });
 
@@ -226,35 +214,11 @@ impl RxSocket {
     }
 
     #[inline(always)]
-    fn fill(&self) -> u32 {
-        let mut index: u32 = 0;
-        let size = std::cmp::min(self.descriptor_buffer.len(), self.rx_ring.size);
-
-        let available = self.umem.fill_ring().reserve(size, &mut index);
-        if available > 0 {
-            for _ in 0..available {
-                let address = self.umem.fill_ring().fill_address(index);
-
-                // # Safety
-                // It is safe to call `unwrap()` on descriptor buffer because the buffer is
-                // guaranteed to be larger than the ring size.
-                unsafe { *address = self.descriptor_buffer.pop().unwrap() }
-                index += 1;
-            }
-
-            self.umem.fill_ring().submit(available);
-        }
-
-        if self.umem.fill_ring().needs_wakeup() {
+    pub fn rx_burst(&mut self, buffer: &mut VecDeque<Descriptor>) -> u32 {
+        self.umem.fill(&self.descriptor_buffer);
+        if self.umem.needs_wakeup() {
             self.socket.poll_fd();
         }
-
-        available
-    }
-
-    #[inline(always)]
-    pub fn rx_burst(&mut self, buffer: &mut VecDeque<Descriptor>) -> u32 {
-        self.fill();
 
         let mut index: u32 = 0;
         let batch_size = std::cmp::min(buffer.capacity() as u32, self.rx_ring.size);
@@ -298,30 +262,6 @@ impl TxSocket {
     }
 
     #[inline(always)]
-    fn complete(&self, size: u32) -> u32 {
-        if self.tx_ring.needs_wakeup() {
-            self.socket.send_fd();
-        }
-
-        let mut index: u32 = 0;
-
-        let available = self.umem.completion_ring().peek(size, &mut index);
-        if available > 0 {
-            for _ in 0..available {
-                let address = self.umem.completion_ring().complete_address(index);
-                unsafe {
-                    self.descriptor_buffer.push(*address);
-                }
-                index += 1;
-            }
-
-            self.umem.completion_ring().release(available);
-        }
-
-        available
-    }
-
-    #[inline(always)]
     pub fn tx_burst(&mut self, buffer: &mut VecDeque<Descriptor>) -> u32 {
         let mut index: u32 = 0;
         let batch_size = buffer.len() as u32;
@@ -341,7 +281,10 @@ impl TxSocket {
             self.tx_ring.submit(available);
         }
 
-        self.complete(batch_size);
+        if self.tx_ring.needs_wakeup() {
+            self.socket.send_fd();
+        }
+        self.umem.complete(&self.descriptor_buffer);
 
         available
     }
