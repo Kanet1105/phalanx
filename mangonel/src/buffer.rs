b8a45ebf4f45791ed75cc06cbff55558aebd32f8
kanet <zombiedelah@gmail.com>
2024-09-22 21:52:19 +0900

    'Buffer' trait and 'RingBuffer' type

--- mangonel/src/buffer.rs
+++ mangonel/src/buffer.rs
@@ -2,46 +2,61 @@ use std::sync::Arc;
 
 use crossbeam::queue::ArrayQueue;
 
-pub trait Buffer<T>: Send + Sync {
-    fn new(size: usize) -> Self
-    where
-        Self: Sized;
+pub trait Buffer<T> {
+    /// Return the number of items in the buffer.
+    fn count(&self) -> u32;
 
-    fn len(&self) -> u32;
+    /// Return the number of items that can be pushed before [`Buffer::push`]
+    /// starts returning a value.
+    fn free(&self) -> u32;
 
+    /// Return [None] if the buffer is empty.
     fn pop(&self) -> Option<T>;
 
+    /// Return a previous value if the buffer is full.
     fn push(&self, value: T) -> Option<T>;
 }
 
-pub struct DescriptorBuffer<T>(Arc<ArrayQueue<T>>);
-
-unsafe impl<T> Send for DescriptorBuffer<T> {}
-
-unsafe impl<T> Sync for DescriptorBuffer<T> {}
+pub struct DescriptorBuffer<T> {
+    inner: Arc<ArrayQueue<T>>,
+}
 
 impl<T> Clone for DescriptorBuffer<T> {
     fn clone(&self) -> Self {
-        Self(self.0.clone())
+        Self {
+            inner: self.inner.clone(),
+        }
     }
 }
 
 impl<T> Buffer<T> for DescriptorBuffer<T> {
-    fn new(size: usize) -> Self {
-        let ring = ArrayQueue::<T>::new(size as usize);
-
-        Self(Arc::new(ring))
+    #[inline(always)]
+    fn count(&self) -> u32 {
+        self.inner.len() as u32
     }
 
-    fn len(&self) -> u32 {
-        self.0.len() as u32
+    #[inline(always)]
+    fn free(&self) -> u32 {
+        (self.inner.capacity() - self.inner.len()) as u32
     }
 
+    #[inline(always)]
     fn pop(&self) -> Option<T> {
-        self.0.pop()
+        self.inner.pop()
     }
 
+    #[inline(always)]
     fn push(&self, value: T) -> Option<T> {
-        self.0.force_push(value)
+        self.inner.force_push(value)
+    }
+}
+
+impl<T> DescriptorBuffer<T> {
+    pub fn new(capacity: usize) -> Self {
+        let ring_buffer = ArrayQueue::<T>::new(capacity);
+
+        Self {
+            inner: Arc::new(ring_buffer),
+        }
     }
 }
