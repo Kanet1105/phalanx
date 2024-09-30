373de1da03d9244eba462ceebc38e9e92434ae3b
Kanet <zombiedelah@gmail.com>
2024-09-30 15:01:20 +0900

    libxdp related modules moved to 'mangonel-libxdp-rs'

--- mangonel/src/lib.rs
+++ mangonel/src/lib.rs
@@ -1,7 +1,1 @@
-pub mod buffer;
-pub mod descriptor;
-pub mod mmap;
-pub mod ring;
-pub mod socket;
-pub mod umem;
-pub mod util;
+
--- mangonel/src/main.rs
+++ mangonel/src/main.rs
@@ -6,8 +6,6 @@ use std::{
     },
 };
 
-use mangonel::{descriptor::Descriptor, socket::SocketBuilder};
-
 fn main() {
     let running = Arc::new(AtomicBool::new(true));
     ctrlc::set_handler({
@@ -18,23 +16,24 @@ fn main() {
     })
     .unwrap();
 
-    let interface_name = "br0";
-    let queue_id = 0;
-    let config = SocketBuilder::default();
-    let (mut receiver, mut sender) = config.build(interface_name, queue_id).unwrap();
+    // let interface_name = "br0";
+    // let queue_id = 0;
+    // let config = SocketBuilder::default();
+    // let (mut receiver, mut sender) = config.build(interface_name,
+    // queue_id).unwrap();
 
-    let mut receiver_buffer = VecDeque::<Descriptor>::with_capacity(64);
-    let mut sender_buffer = VecDeque::<Descriptor>::with_capacity(64);
-    while running.load(Ordering::SeqCst) {
-        let received = receiver.rx_burst(&mut receiver_buffer);
-        if received > 0 {
-            for _ in 0..received {
-                let mut descriptor = receiver_buffer.pop_front().unwrap();
-                descriptor.get_data();
-                sender_buffer.push_back(descriptor);
-            }
+    // let mut receiver_buffer = VecDeque::<Descriptor>::with_capacity(64);
+    // let mut sender_buffer = VecDeque::<Descriptor>::with_capacity(64);
+    // while running.load(Ordering::SeqCst) {
+    //     let received = receiver.rx_burst(&mut receiver_buffer);
+    //     if received > 0 {
+    //         for _ in 0..received {
+    //             let mut descriptor = receiver_buffer.pop_front().unwrap();
+    //             descriptor.get_data();
+    //             sender_buffer.push_back(descriptor);
+    //         }
 
-            sender.tx_burst(&mut sender_buffer);
-        }
-    }
+    //         sender.tx_burst(&mut sender_buffer);
+    //     }
+    // }
 }
