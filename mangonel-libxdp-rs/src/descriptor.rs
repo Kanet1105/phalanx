4967e953a985e4331942d0c037e1463a36c225d4
Kanet <zombiedelah@gmail.com>
2024-10-02 11:07:00 +0900

    'get_data()' returns mutable slice of packet

--- mangonel-libxdp-rs/src/descriptor.rs
+++ mangonel-libxdp-rs/src/descriptor.rs
@@ -1,5 +1,4 @@
 use mangonel_libxdp_sys::xdp_desc;
-use pnet::packet::{ethernet::MutableEthernetPacket, ipv4::MutableIpv4Packet};
 
 use crate::umem::Umem;
 
@@ -34,11 +33,9 @@ impl Descriptor {
     }
 
     #[inline(always)]
-    pub fn get_data(&mut self) {
+    pub fn get_data(&mut self) -> &mut [u8] {
         let offset = self.umem.get_data(self.address) as *mut u8;
         let data = unsafe { std::slice::from_raw_parts_mut(offset, self.length as usize) };
-        // let ethernet_frame = MutableEthernetPacket::new(data).unwrap();
-        let ipv4_packet = MutableIpv4Packet::new(&mut data[14..]).unwrap();
-        println!("{:?}", ipv4_packet);
+        data
     }
 }
